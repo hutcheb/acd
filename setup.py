2d876e7863a9507ee335e4edccd6474e214aabd9
Ben Hutcheson <ben.hutche@gmail.com>
2024-07-06 16:44:13 +0200

    chore: New Release

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class install(_install):
 
 setup(
     name="acd-tools",
-    version="0.1a9",
+    version="0.2a0",
     description="Rockwell ACD File Tools",
     classifiers=[
         "Development Status :: 3 - Alpha",
